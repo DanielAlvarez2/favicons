a1c2f9d8e21135c53850a881ff2f53b7b57b090c
Daniel Alvarez <daniel.yllanes@hotmail.com>
2025-04-03 13:40:35 -0400

    start over

--- server/server.js
+++
@@ -1,6 +0,0 @@
-import express from 'express'
-const app = express()
-const PORT = process.env.PORT || 9103
-app.use(express.static('../dist'))
-app.listen(PORT, console.log(`Server Running: ${PORT}`))
-app.get('/', async (req, res)=>res.sendFile('index.html'))
